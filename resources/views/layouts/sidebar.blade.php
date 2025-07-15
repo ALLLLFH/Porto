7aeb2b63672f63d942f1e6a9e67803df6b3a6c87
ALFAN FARHAN HADI <alfanfarhanhadi4@gmail.com>
2025-07-15 20:04:49 +0700

    membuat new tab

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 {{-- resources/views/layouts/sidebar.blade.php --}}
 <aside
     :class="sidebarCollapsed ? 'w-20' : 'w-64'"
-    class="hidden sm:block fixed top-0 left-0 h-screen bg-white/40 shadow p-4  flex-col justify-between z-30 transition-all duration-300">
+    class="hidden sm:flex fixed top-0 left-0 h-screen bg-white/40 shadow p-4 flex flex-col justify-between z-30 transition-all duration-300">
     <div>
         <button @click="sidebarCollapsed = !sidebarCollapsed"
             class="mb-6 flex items-center justify-end w-full text-gray-500 hover:text-gray-700 focus:outline-none">
@@ -24,7 +24,7 @@
                 </li>
                 @if(Auth::user()->portfolio)
                     <li>
-                        <a href="{{ route('portofolio.show', Auth::user()->portfolio) }}"
+                        <a href="{{ route('portofolio.show', Auth::user()->portfolio) }}" target="_blank"
                             class="flex items-center p-2 text-base font-normal text-stone-700 rounded-lg hover:bg-blue-200 group hover:text-blue-700 transition-all duration-200">
                             <i class="fa-solid fa-address-card text-lg"></i>
                             <span class="ml-3" x-show="!sidebarCollapsed" x-transition>Portofolio</span>
@@ -36,7 +36,7 @@
     </div>
 
     @auth
-    <div class="pt-4 border-t border-gray-300 mt-auto">
+    <div class="pt-4 border-t border-gray-300">
         <form method="POST" action="{{ route('logout') }}">
             @csrf
             <button type="submit"
